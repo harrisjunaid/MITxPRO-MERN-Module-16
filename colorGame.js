d267aeff6c736f27ba3a40e10194bf549e333a32
Harris Junaid <harris.junaid@gmail.com>
2023-03-07 16:09:47 -0500

    16-9_a

--- colorGame.js
+++ colorGame.js
@@ -6,28 +6,29 @@ const Square = ({id, player}) => {
   const getRandomColor = () => palet[Math.floor(Math.random()*3)];
   return (
     // change color of Square on click
-    <button onClick={e => {
-      setColor(getRandomColor());
-      e.target.style.background = color;
-    }}
-    >
+    <button onClick={() => alert(`I'm Square ${id}`)}>
       <h1>{player}</h1>
     </button>
   )
 }
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
+  const [mounted, setMounted] = React.useState(true);
   let status = `Player ${player}`;
+  const toggle = () => setMounted(!mounted);
   function renderSquare(i) {
     return <Square id={i} player={player}></Square>;
   }
   return (
     <div className="game-board">
       <div className="grid-row">        
-        {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+        {mounted && renderSquare(0)}
+        {mounted && renderSquare(1)}
+        {mounted && renderSquare(2)}
       </div>
       <div id="info">
-        <h1>{status}</h1>
+        <button onClick={toggle}>Show/Hide Row</button>
+        <h1>Turn of player {player}</h1>
       </div>
     </div>
   );

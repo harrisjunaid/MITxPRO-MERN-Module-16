c3bd32434623eb1f9c26c83010de8eb7250d4957
Harris Junaid <harris.junaid@gmail.com>
2023-03-11 07:13:44 -0500

    16-5_a

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,6 @@
-// const Square = () => {
-//   return (
-//     <button></button>
-//   )
-// }
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
-  function renderSquare(i) {
-    return <Square></Square>;
-  }
   return (
     <div className="game-board">
       <div className="grid-row">        
